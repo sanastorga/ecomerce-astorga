6456bd481cb5e45a926d9975edd0cc995bc473a3
Santiago Astorga <sanastorga@gmail.com>
2022-03-27 20:22:10 -0300

    container list y cart separado final

--- src/containers/ItemListContainer.js
+++ src/containers/ItemListContainer.js
@@ -1,9 +1,10 @@
+import { autocompleteClasses } from "@mui/material";
 import React from "react";
 
-const ItemListContainer = (props) => {
-    console.log(props);
+const ItemListContainer = ({saludo}) => {
+    console.log(saludo);
     return(
-        <h1>{props.saludo} </h1>
+        <h1 style={{color: "red", backgroundColor: "blue", fontSize: 50, letterSpacing: 3}}>{saludo} </h1>
     )
 }
 
