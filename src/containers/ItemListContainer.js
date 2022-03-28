b3941967796b523716a56e2ac2eeb3a4e210f355
Santiago Astorga <sanastorga@gmail.com>
2022-03-28 17:08:11 -0300

    itemCount

--- src/containers/ItemListContainer.js
+++ src/containers/ItemListContainer.js
@@ -1,10 +1,20 @@
-import { autocompleteClasses } from "@mui/material";
 import React from "react";
+import ItemCount from "../components/ItemCount";
 
 const ItemListContainer = ({saludo}) => {
-    console.log(saludo);
+    /* console.log(saludo); */
+
+    
+
+    const Informar = (contador) =>{
+        alert( contador )
+    }
+
     return(
+        <>
         <h1 style={{color: "red", backgroundColor: "blue", fontSize: 50, letterSpacing: 3}}>{saludo} </h1>
+        <ItemCount stock = {5} initial = {1} informar= {Informar} />
+        </>
     )
 }
 
